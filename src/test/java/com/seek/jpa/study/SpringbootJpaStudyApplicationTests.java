df9f0139b947303142c02e43935fddd9a70dedac
an-in-seek <dktmrorl@naver.com>
2024-07-20 13:19:02 +0900

    프로젝트 최초 커밋

---
+++ src/test/java/com/seek/jpa/study/SpringbootJpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seek.jpa.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
